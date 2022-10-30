4fc77cde8f5895706d248f3d92bc4163174ffcbb
child_duckling <19170969+child-duckling@users.noreply.github.com>
2022-10-30 02:46:35 -0700

    move api to generation

--- pages/cms.js
+++ pages/cms.js
@@ -89,10 +89,43 @@ export async function getStaticPaths() {
 export async function getStaticProps() {
     // Call an external API endpoint to get posts.
     // You can use any data fetching library
-    const request = await fetch(
-        `http://127.0.0.1:3000/api/cms`
-    );
-    let cms = await request.json();
+    let districts = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+    ];
+    let data = [];
+    //console.log("Checking", districts.length, "Districts");
+    for (var i = 0; i <= districts.length - 1; i++) {
+        let short;
+        if (
+            String(districts[i]).charAt("0") == "0" &&
+            String(districts[i]) != "0"
+        ) {
+            short = districts[i].charAt(1);
+        } else {
+            short = districts[i];
+        }
+
+        const res = await fetch(
+            `https://cwwp2.dot.ca.gov/data/d${short}/cms/cmsStatusD${districts[i]}.json`
+        );
+
+        let cameras = await res.json();
+        //console.log("+", cameras.data.length);
+        data.push(...cameras.data);
+    }
+
+    let cms = data
     //console.log(cms)
     //let district = params.district
     // By returning { props: { posts } }, the Blog component
