2acf891036ad72c3608b259ac20d09241d877156
Quacksire <sam@quacksire.dev>
2023-02-15 10:35:49 -0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,6 @@
 import {Button, Grid, Spacer, Text} from "@nextui-org/react";
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 export default function Footer() {
 
     return (
@@ -7,7 +8,7 @@ export default function Footer() {
             <Spacer y={2} />
             <Grid.Container justify="center">
                 <Grid>
-                    Made with <span style={{color: "red"}}>❤</span> by <a href="https://quacksire.dev">Quacksire</a>
+                    Made with <span style={{color: "red"}}>❤</span> by <Link href="https://quacksire.dev" target={'_blank'} rel={'noreferrer'}>Quacksire</Link>
                 </Grid>
             </Grid.Container>
             <Spacer y={1} />
